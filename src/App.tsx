7b6b464bb6dda0aad0e5cfe0fc765854533aa1dd
MaksimLuzhynski <60569114+MaksimLuzhynski@users.noreply.github.com>
2021-06-17 17:32:24 +0300

    19 (route, browser-router)

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,26 @@ import { Dialogs } from './components/Dialogs/Dialogs';
 import { Header } from './components/Header/Header';
 import { Navbar } from './components/Navbar/Navbar';
 import { Profile } from './components/Profile/Profile';
-
+import { BrowserRouter, Route } from 'react-router-dom';
+import { News } from './components/News/News';
+import { Music } from './components/Music/Music';
+import { Settings } from './components/Settings/Settings';
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-        <Dialogs />
-        {/* <Profile /> */}
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route path='/profile' component={Profile}></Route>
+          <Route path='/dialogs' component={Dialogs}></Route>
+          <Route path='/news' component={News}></Route>
+          <Route path='/music' component={Music}></Route>
+          <Route path='/settings' component={Settings}></Route>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,11 +5,11 @@ import classes from './Navbar.module.css'
 export function Navbar() {
     return (
         <nav className={classes.nav}>
-            <div className={classes.item}><a>Profile</a></div>
-            <div className={classes.item}><a>Messanges</a></div>
-            <div className={classes.item}><a>News</a></div>
-            <div className={classes.item}><a>Music</a></div>
-            <div className={classes.item}><a>Settings</a></div>
+            <div className={classes.item}><a href="/profile">Profile</a></div>
+            <div className={classes.item}><a href="/dialogs">Messanges</a></div>
+            <div className={classes.item}><a href="/news">News</a></div>
+            <div className={classes.item}><a href="/music">Music</a></div>
+            <div className={classes.item}><a href="/settings">Settings</a></div>
         </nav>
     )
 }
