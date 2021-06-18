4503d8190bf4b6c5c8db78ed482e13a5f4644849
MaksimLuzhynski <60569114+MaksimLuzhynski@users.noreply.github.com>
2021-06-18 14:22:16 +0300

    22 (fix Dialogs page)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import classes from './Dialogs.module.css'
 
 export function Dialogs() {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItem}>
-                <div className={classes.dialog + ' ' + classes.active}> Max  </div> {/* очень важно поставить пробел между кавычками */}
-                <div className={classes.dialog}> Ales  </div>
-                <div className={classes.dialog}> Bob  </div>
-                <div className={classes.dialog}> Marta  </div>
-                <div className={classes.dialog}> Victor  </div>
+                <div className={classes.dialog + ' ' + classes.active}> {/* очень важно поставить пробел между кавычками */}
+                    <NavLink to="/dialogs/1"> Max</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/2"> Ales</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/3"> Bob</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/4"> Marta</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/5">  Victor</NavLink>
+                </div>
             </div>
             <div className={classes.messages}>
                 <div className={classes.message}> hi  </div>
