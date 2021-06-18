ea794281af43bfa36d4bf84f8f5af71734ac3a18
MaksimLuzhynski <60569114+MaksimLuzhynski@users.noreply.github.com>
2021-06-18 12:40:05 +0300

    21 (fix Dialogs page)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import classes from './Dialogs.module.css'
 
 export function Dialogs() {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItem}>
+                <div className={classes.dialog + ' ' + classes.active}> Max  </div> {/* очень важно поставить пробел между кавычками */}
+                <div className={classes.dialog}> Ales  </div>
+                <div className={classes.dialog}> Bob  </div>
+                <div className={classes.dialog}> Marta  </div>
+                <div className={classes.dialog}> Victor  </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}> hi  </div>
+                <div className={classes.message}> feerfe  </div>
+                <div className={classes.message}> Maefefx  </div>
+                <div className={classes.message}> Mefefax  </div>
+            </div>
         </div>)
 }
