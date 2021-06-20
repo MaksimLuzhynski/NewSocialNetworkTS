9b00a782a3c119a73c7b1276dc0a75ad23100881
MaksimLuzhynski <60569114+MaksimLuzhynski@users.noreply.github.com>
2021-06-20 15:12:38 +0300

    24 (move data aside)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -6,9 +6,16 @@ type DialogItemPropsType = {
     name: string
     id: number
 }
-
 type MessagePropsType = {
     message: string
+}
+type DialogsDataType = {
+    id: number
+    name: string
+}
+type MessageDataType = {
+    id:number
+    name:string
 }
 
 export function DialogItem(props: DialogItemPropsType) {
@@ -24,22 +31,38 @@ export function Message(props: MessagePropsType) {
     )
 }
 
+let DialogsData: Array<DialogsDataType> = [
+    { id: 1, name: "Max" },
+    { id: 2, name: "Ales" },
+    { id: 3, name: "Bob" },
+    { id: 4, name: "Marta" },
+    { id: 5, name: "Victor" },
+]
+
+let MessageData:Array<MessageDataType> = [
+    { id: 0, name: "Hi" },
+    { id: 1, name: "How are you?" },
+    { id: 2, name: "Yo!" },
+    { id: 3, name: "Hellow" },
+    { id: 4, name: "Hey" },
+]
+// не забудь про типизацию
 export function Dialogs() {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItem}>
-                <DialogItem name="Max" id={1} />
-                <DialogItem name="Ales" id={2} />
-                <DialogItem name="Bob" id={3} />
-                <DialogItem name="Marta" id={4} />
-                <DialogItem name="Victor" id={5} />
+                <DialogItem name={DialogsData[0].name} id={DialogsData[0].id} />
+                <DialogItem name={DialogsData[1].name} id={DialogsData[1].id} />
+                <DialogItem name={DialogsData[2].name} id={DialogsData[2].id} />
+                <DialogItem name={DialogsData[3].name} id={DialogsData[3].id} />
+                <DialogItem name={DialogsData[4].name} id={DialogsData[4].id} />
             </div>
             <div className={classes.messages}>
-                <Message message="Hi"/>
-                <Message message="How are you?"/>
-                <Message message="Yo!"/>
-                <Message message="Hellow"/>
-                <Message message="Hey"/>
+                <Message message={MessageData[0].name} />
+                <Message message={MessageData[1].name} />
+                <Message message={MessageData[2].name} />
+                <Message message={MessageData[3].name} />
+                <Message message={MessageData[4].name} />
             </div>
         </div>)
 }
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,6 +2,19 @@ import React from 'react';
 import classes from './MyPosts.module.css';
 import { Post } from './Post/Post';
 
+type PostDataType={
+    id:number
+    message:string
+    likeCounter: number
+}
+
+let PostData:Array<PostDataType> = [
+    { id: 0, message: "Hey!", likeCounter: 0 },
+    { id: 1, message: "How are you?", likeCounter: 15 },
+    { id: 2, message: "Hellow", likeCounter: 3 },
+    { id: 3, message: "Yo!!!", likeCounter: 37 },
+]
+
 export function MyPosts() {
     return (
         <div className={classes.postsBlock}>
@@ -14,22 +27,10 @@ export function MyPosts() {
                     <button>Add post</button>
                 </div>
                 <div className={classes.posts}>
-                    <Post
-                        message="Hey!"
-                        likeCounter={0}
-                    />
-                    <Post
-                        message="How are you?"
-                        likeCounter={15}
-                    />
-                    <Post
-                        message="Hellow"
-                        likeCounter={3}
-                    />
-                    <Post
-                        message="Yo!!!"
-                        likeCounter={37}
-                    />
+                    <Post message={PostData[0].message} likeCounter={PostData[0].likeCounter} />
+                    <Post message={PostData[1].message} likeCounter={PostData[1].likeCounter} />
+                    <Post message={PostData[2].message} likeCounter={PostData[2].likeCounter} />
+                    <Post message={PostData[3].message} likeCounter={PostData[3].likeCounter} />
                 </div>
             </div>
         </div>
