bee0e58639c7280b4fa57651321ac3d3c99f4043
MaksimLuzhynski <60569114+MaksimLuzhynski@users.noreply.github.com>
2021-06-23 15:39:03 +0300

    31 (onClick, ref)

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,24 +3,32 @@ import { PostsType } from '../../../redux/state';
 import classes from './MyPosts.module.css';
 import { Post } from './Post/Post';
 
-type MyPostsPropsType={
-    posts:Array<PostsType>
+type MyPostsPropsType = {
+    posts: Array<PostsType>
 }
 
-export function MyPosts(props:MyPostsPropsType) {
-    
-    //типизация???
+export function MyPosts(props: MyPostsPropsType) {
+
     let postsElements = props.posts.map(post => <Post message={post.message} likeCounter={post.likeCounter} />)
+
+    let newPostElement = React.createRef<HTMLTextAreaElement>();
+
+    let addPost = () => {
+        if (newPostElement.current) {
+            let text = newPostElement.current.value;
+            alert(text);
+        }
+    }
 
     return (
         <div className={classes.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
                 <div className={classes.posts}>
                     {postsElements}
